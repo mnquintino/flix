8b727e34018a0aa1edbca46cf8094a2457f9a114
Manara <mnquintino@gmail.com>
2022-02-02 18:44:37 -0300

    12.Forms:Editing Records

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,15 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params = params.required(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
 end

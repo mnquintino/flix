05c3b2d9665f04c7bc721edcd33fa449789e8b32
Manara <mnquintino@gmail.com>
2022-02-03 11:25:04 -0300

    12, 13, 14 - Forms Create and Destroy, Partials

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,16 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+  end
+
   def show
     @movie = Movie.find(params[:id])
   end
@@ -14,8 +24,21 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    movie_params = params.required(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
     @movie.update(movie_params)
     redirect_to @movie
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_url
+    
+  end
+
+private
+
+  def movie_params
+    params.required(:movie).
+      permit(:title, :description, :rating, :released_on, :total_gross)
   end
 end

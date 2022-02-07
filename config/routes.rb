199efeb7e13482bd0413e575e6cb995556dffcc0
Manara <mnquintino@gmail.com>
2022-02-07 18:33:41 -0300

    21 - 23: One to many

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-
+  
     root "movies#index"
 
-    resources :movies
+    resources :movies do
+      resources :reviews
+    end
     # get "movies" => "movies#index"
     # get "movies/:id" => "movies#show", as: "movie"
     # get "movies/:id/edit" => "movies#edit", as: "edit_movie"

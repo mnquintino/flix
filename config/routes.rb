00f88f44acc1db6d18e6b49d5ab3aae165799343
Manara <mnquintino@gmail.com>
2022-02-08 15:26:24 -0300

    26 - 28. User account

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   
+  
     root "movies#index"
 
     resources :movies do
       resources :reviews
     end
+
+    resources :users
+    get "signup" => "users#new"
+
     # get "movies" => "movies#index"
     # get "movies/:id" => "movies#show", as: "movie"
     # get "movies/:id/edit" => "movies#edit", as: "edit_movie"

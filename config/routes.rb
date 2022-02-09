ba2e8fb0c575914949bc90ddd408f2335a6204a0
Manara <mnquintino@gmail.com>
2022-02-09 11:59:59 -0300

    29-32.Sign in/up/out

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     resources :users
     get "signup" => "users#new"
 
+    resource :session, only:[:new, :create, :destroy]
+
     # get "movies" => "movies#index"
     # get "movies/:id" => "movies#show", as: "movie"
     # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
